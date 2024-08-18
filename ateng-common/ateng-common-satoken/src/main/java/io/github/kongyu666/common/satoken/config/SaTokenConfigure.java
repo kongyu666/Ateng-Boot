15b55d27464298b38812910695f622bbe0497ff3
孔余 <2385569970@qq.com>
2024-08-18 10:56:53 +0800

    新增：/actuator/shutdown需要验证

--- ateng-common/ateng-common-satoken/src/main/java/io/github/kongyu666/common/satoken/config/SaTokenConfigure.java
+++ ateng-common/ateng-common-satoken/src/main/java/io/github/kongyu666/common/satoken/config/SaTokenConfigure.java
@@ -1,11 +1,16 @@
 package io.github.kongyu666.common.satoken.config;
 
+import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.httpauth.basic.SaHttpBasicUtil;
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.stp.StpUtil;
+import io.github.kongyu666.common.core.constant.AppCodeEnum;
 import io.github.kongyu666.common.core.factory.YmlPropertySourceFactory;
+import io.github.kongyu666.common.core.utils.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,5 +40,19 @@ public class SaTokenConfigure implements WebMvcConfigurer {
                 )
                 .addPathPatterns("/**")
                 .excludePathPatterns("/actuator/**", "/demo/**");
+    }
+
+    /*
+    注册 Sa-Token 全局过滤器
+     */
+    @Bean
+    public SaServletFilter getSaServletFilter() {
+        return new SaServletFilter()
+                .addInclude("/actuator/shutdown")
+                .setAuth(obj -> SaHttpBasicUtil.check("admin:Admin@123"))
+                .setError(e -> {
+                    log.error(e.getMessage());
+                    return Result.error(AppCodeEnum.OPERATION_CANCELED.getCode(), AppCodeEnum.OPERATION_CANCELED.getDescription());
+                });
     }
 }
